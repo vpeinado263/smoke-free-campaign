374cbdad60ebae6c0867209a6188761968d60b57
Victor Peinado <vpeinado263@gmail.com>
2024-10-23 23:56:56 -0300

    Agregar contenido a about,js

---
+++ src/pages/about.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const about = () => {
+  return (
+    <div>
+        <Headers></Headers>
+        <main>
+            <h2>La Adiccion ala nicotina</h2>
+            <p>
+            La nicotina es una droga que se encuentra en los cigarrillos y que genera una fuerte dependencia.
+            Fumar cigarrillos puede llevar a enfermedades como el cáncer, problemas respiratorios, y enfermedades
+            cardíacas. El consumo prolongado de tabaco es extremadamente dañino para la salud.
+            </p>
+        </main>
+    </div>
+  )
+}
+
+export default about;
