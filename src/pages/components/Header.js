0b93f23eec6b5ea48bbccc6634442a97f928b1fd
Victor Peinado <vpeinado263@gmail.com>
2024-10-23 23:17:29 -0300

    Crear archivo about.js

--- src/pages/components/Header.js
+++ src/pages/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <header>
-        <h1>Consenticiacion Sobre el habito de fumar</h1>
+        <h1>El Hábito de fumar</h1>
         <nav>
             <a href='/'>Inicio</a>|<a href='/about'>Aceca de la adiccion</a>
         </nav>
