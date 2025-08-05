f71b5ad3ced5080deaa1459436804ae6cbb7b96f
Victor Peinado <vpeinado263@gmail.com>
2025-08-05 09:20:05 -0300

    Modificar titulo principal

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 const Header = () => {
   return (
     <header>
-        <h1>Campania sin Tabaco</h1>
+        <h1>Campaña sin Tabaco</h1>
         <nav>
             <Link href='/'>Inicio</Link> | <Link href='/about'>Aceca de la adiccion</Link>
         </nav>
